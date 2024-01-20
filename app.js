be68bc976946641f35318c4c5adf64a508853ce1
Adriana <103216065+facundocaro1826@users.noreply.github.com>
2024-01-20 01:16:07 -0300

    realice la estructura inicial de trabajo utilizando node.js y express

---
+++ app.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3030;
+
+/* config */
+app.use(express.static('public'));
+
+/* routes */
+const createPath = (filename) => path.join(__dirname, `./views/${filename}.html`);
+
+app.get('/', (req, res)=>{
+    res.sendFile(createPath('index'))  //home => Florencia
+})
+
+app.get('/productDetail', (req, res)=>{
+    res.sendFile(createPath('(productDetail')) // Gaston 
+})
+
+app.get('/productCar', (req, res)=>{
+    res.sendFile(createPath('productCar'))  // Sofia
+})
+
+app.get('/register', (req, res)=>{
+    res.sendFile(createPath('register'))     //Pato
+})
+
+app.get('/login', (req, res)=>{
+    res.sendFile(createPath('login'))     // Adriana
+})
+
+
+
+
+
+
+
+/* server */
+app.listen(port, ()=> console.log(`http://localhost:${(port)}`))
