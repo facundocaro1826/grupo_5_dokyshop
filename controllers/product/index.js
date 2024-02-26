c3059e9c7ce8fb212385956a28dd7cdb9c9cbf81
GastonAyala <gastonayalagimenez@gmail.com>
2024-02-26 05:06:57 -0300

    agrego la vista edit y create product con sus respectivos controladores, rutas  y estilos

--- controllers/product/index.js
+++ controllers/product/index.js
@@ -1,6 +1,7 @@
 module.exports = {
    product: require("./product.controller"),
    detail: require("./detail.controller"),
+   dashboard: require("./dashboard.controllers"),
    edit: require("./editProduct.controllers"),
-   dashboard: require("./dashboard.controllers")
+   create: require("./createProduct.controller")
 }
--- routers/product.routes.js
+++ routers/product.routes.js
@@ -5,8 +5,9 @@ const productController = require('../controllers/product')
 
 router.get("/", productController.product)  //  /productos
 router.get("/detalle-de-producto", productController.detail);
-router.get("/editar-producto", productController.edit);
 router.get("/panel", productController.dashboard);
+router.get("/editar-producto", productController.edit);
+router.get("/crear-producto", productController.create);
 
 
 
