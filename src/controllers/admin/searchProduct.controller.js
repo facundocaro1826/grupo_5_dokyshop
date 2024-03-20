a9739dcee93aa45f6551785ddc2b88da8ad749ca
GastonAyala <gastonayalagimenez@gmail.com>
2024-03-20 04:26:22 -0300

    agregue la funcionalidad de filtrado de productos por categoria en el listado de productos del Dashboard, agregue redireccionamiento al detalle del producto en el boton info de cada producto

--- src/controllers/admin/searchProduct.controller.js
+++ src/controllers/admin/searchProduct.controller.js
@@ -1,11 +1,11 @@
 const { loadData } = require('../../data');
 
 module.exports = (req, res) => {
-    const { dashboardKeywords } = req.query
+    const { dashboardKeywords, filterSelect } = req.query
     const products = loadData()
-    const productsFilter = products.filter(p => p.title.toLowerCase().includes(dashboardKeywords.toLowerCase()) || p.description.toLowerCase().includes(dashboardKeywords.toLowerCase()) || p.categorySelect.toLowerCase().includes(dashboardKeywords.toLowerCase()))
+    const productsFilter = products.filter(p => p.title.toLowerCase().includes(dashboardKeywords?.toLowerCase()) || p.description.toLowerCase().includes(dashboardKeywords?.toLowerCase()) || p.categorySelect.toLowerCase().includes(filterSelect?.toLowerCase()))
 
-    res.render("admin/searchProducResults", { products: productsFilter, dashboardKeywords}, (err, contentView) => {
+    res.render("admin/searchProducResults", { products: productsFilter, dashboardKeywords, filterSelect}, (err, contentView) => {
         err && res.send(err.message)
 
         res.render("partials/dashboard", {contentView})
